86722ee9e84f8152668c4f2580d720ef7d03846a
Sylvain Corlay <sylvain.corlay@gmail.com>
2016-12-06 01:30:45 +0100

    Release 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'jupyter_sphinx',
-    version = '0.1.0',
+    version = '0.0.1',
     author = 'Jupyter Development Team',
     author_email = 'jupyter@googlegroups.com',
     description = 'Jupyter Sphinx Extensions',
