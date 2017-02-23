3019d9f8c74f5a9dbba5e3f21585727ee6a79bde
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-02-23 15:36:56 +0100

    Release 0.0.3

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name = 'jupyter_sphinx',
-    version = '0.0.2',
+    version = '0.0.3',
     author = 'Jupyter Development Team',
     author_email = 'jupyter@googlegroups.com',
     description = 'Jupyter Sphinx Extensions',
     license = 'BSD',
     packages = ['jupyter_sphinx'],
-    install_requires = ['Sphinx>=0.6', 'ipywidgets>=6.0.0b6'],
+    install_requires = ['Sphinx>=0.6', 'ipywidgets>=6.0.0rc2'],
 )
