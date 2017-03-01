05c7e85025834110d34a0ed5663c2a0dabe48bcb
Sylvain Corlay <sylvain.corlay@gmail.com>
2017-03-01 10:33:33 +0100

    Release 0.1.0 - update to ipywidgets 6.0 and set up version string

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+name = 'jupyter_sphinx'
+
+version_ns = {}
+with open(os.path.join(here, name, '_version.py')) as f:
+    exec(f.read(), {}, version_ns)
 
 setup(
-    name = 'jupyter_sphinx',
-    version = '0.0.3',
+    name = name,
+    version = version_ns['__version__'],
     author = 'Jupyter Development Team',
     author_email = 'jupyter@googlegroups.com',
     description = 'Jupyter Sphinx Extensions',
     license = 'BSD',
     packages = ['jupyter_sphinx'],
-    install_requires = ['Sphinx>=0.6', 'ipywidgets>=6.0.0rc2'],
+    install_requires = ['Sphinx>=0.6', 'ipywidgets>=6.0.0'],
 )
