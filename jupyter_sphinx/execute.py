e18b5550ff67a59c7dfb66d0f1f7b83d7275fc30
Bas Nijholt <basnijholt@gmail.com>
2018-10-18 18:24:59 +0200

    add javascript support
    With this we can (for example) include HoloViews, Bohek, or plotly plots in the docs.
    
    This is already used in https://adaptive.readthedocs.io

--- jupyter_sphinx/execute.py
+++ jupyter_sphinx/execute.py
@@ -432,6 +432,12 @@ def cell_output_to_nodes(cell, data_priority, dir):
                     text=data,
                     rawsource=data,
                 ))
+            elif mime_type == 'application/javascript':
+                to_add.append(docutils.nodes.raw(
+                    text='<script type="{mime_type}">{data}</script>'
+                         .format(mime_type=mime_type, data=data),
+                    format='html',
+                ))
             elif mime_type == WIDGET_VIEW_MIMETYPE:
                 to_add.append(JupyterWidgetViewNode(data))
 
@@ -558,6 +564,7 @@ def setup(app):
         'jupyter_execute_data_priority',
         [
             WIDGET_VIEW_MIMETYPE,
+            'application/javascript',
             'text/html',
             'image/svg+xml',
             'image/png',
