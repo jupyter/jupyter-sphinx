b14d2c2b2dc6be9e301f09ad5987635386c055cd
Anton Akhmerov <anton.akhmerov@gmail.com>
2019-07-09 18:15:53 +0200

    typo and formatting fixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author = 'Jupyter Development Team',
     author_email = 'jupyter@googlegroups.com',
     description = 'Jupyter Sphinx Extensions',
-    project_urls={
+    project_urls = {
         'Bug Tracker': 'https://github.com/jupyter/jupyter-sphinx/issues/',
         'Documentation': 'https://jupyter-sphinx.readthedocs.io',
         'Source Code': 'https://github.com/jupyter/jupyter-sphinx/',
