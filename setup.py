18654dd41c183b65e4e4d25d009a14e23d4495f2
Chris Holdgraf <choldgraf@berkeley.edu>
2020-04-29 11:38:27 -0700

    long description'

--- setup.py
+++ setup.py
@@ -8,12 +8,17 @@ version_ns = {}
 with open(os.path.join(here, name, "_version.py")) as f:
     exec(f.read(), {}, version_ns)
 
+with open(os.path.join(here, "README.md")) as f:
+    description = f.read()
+
 setup(
     name=name,
     version=version_ns["__version__"],
     author="Jupyter Development Team",
     author_email="jupyter@googlegroups.com",
     description="Jupyter Sphinx Extensions",
+    long_description=description,
+    long_description_content_type='text/markdown',
     url="https://github.com/jupyter/jupyter-sphinx/",
     project_urls={
         "Bug Tracker": "https://github.com/jupyter/jupyter-sphinx/issues/",
