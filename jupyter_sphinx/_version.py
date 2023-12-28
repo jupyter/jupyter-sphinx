8da3d1c905f5525f8a751830c83b26d5a11fcffb
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-12-28 13:02:40 +0100

    RLS: 0.5.2 (#252)

--- jupyter_sphinx/_version.py
+++ jupyter_sphinx/_version.py
@@ -6,7 +6,7 @@ import re
 from typing import List
 
 # Version string must appear intact for automatic versioning
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 # Build up version_info tuple for backwards compatibility
 pattern = r"(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)"
