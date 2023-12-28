89d838ce2b4cf40ad1dbbfcd5cfab8a8c1ee2381
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2023-12-28 13:24:24 +0000

    refactor: conf file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,10 +1,10 @@
-#
-# Configuration file for the Sphinx documentation builder.
-#
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+"""
+Configuration file for the Sphinx documentation builder.
 
+For a full list see the documentation: http://www.sphinx-doc.org/en/master/config
+"""
+
+# -- Path setup ----------------------------------------------------------------
 import os
 import sys
 
@@ -12,24 +12,20 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 import jupyter_sphinx  # noqa: E402
 
+# -- Project information -------------------------------------------------------
 project = "Jupyter Sphinx"
 copyright = "2019, Jupyter Development Team"
 author = "Jupyter Development Team"
-
-# The full version, including alpha/beta/rc tags
 release = jupyter_sphinx.__version__
-# The short X.Y version
-version = release[: len(release) - len(release.lstrip("0123456789."))].rstrip(".")
 
+# -- General configuration -----------------------------------------------------
 extensions = ["sphinx.ext.mathjax", "jupyter_sphinx"]
 
+# -- Options for HTML output ---------------------------------------------------
 html_theme = "sphinx_book_theme"
-# html_theme_options = {
-#    "github_user": "jupyter",
-#    "github_repo": "jupyter-sphinx",
-#    "github_banner": True,
-# }
 
-jupyter_sphinx_thebelab_config = {"binderOptions": {"repo": "jupyter/jupyter-sphinx"}}
-
+# -- Options for LaTeX output --------------------------------------------------
 latex_engine = "xelatex"
+
+# -- Jupyter Sphinx options ----------------------------------------------------
+jupyter_sphinx_thebelab_config = {"binderOptions": {"repo": "jupyter/jupyter-sphinx"}}
