cf6b8210aa4b103992950265825c0990d43eabc6
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-01-10 12:50:34 +0000

    fix: use the jupyter orange in the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ extensions = ["sphinx.ext.mathjax", "jupyter_sphinx", "sphinx_design"]
 # -- Options for HTML output ---------------------------------------------------
 html_theme = "sphinx_book_theme"
 html_title = "jupyter-sphinx"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 html_theme_options = {
     "repository_url": "https://github.com/jupyter/jupyter-sphinx",
     "use_repository_button": True,
